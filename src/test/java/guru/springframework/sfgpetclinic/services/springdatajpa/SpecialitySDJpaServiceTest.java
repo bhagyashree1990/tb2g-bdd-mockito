786ccb7470acd3189498243f38c777d2b8c8b069
Bhagyashree Chavan <bhagyashree.chavan90@gmail.com>
2020-09-17 19:50:26 +0530

    Advanced Mockito
    Throwing Exceptions
    Argument Matchers
    Argument Capture
    Answers
    Verify order of interactions
    Verify interaction within specified time
    Verify zero or no more interactions
    Spies

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -11,14 +11,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
 
-    @Mock
+    @Mock(lenient = true)
     SpecialtyRepository specialtyRepository;
 
     @InjectMocks
@@ -46,7 +47,9 @@ class SpecialitySDJpaServiceTest {
 
         //then
         assertThat(foundSpecialty).isNotNull();
-        then(specialtyRepository).should().findById(anyLong());
+        //then(specialtyRepository).should().findById(anyLong());
+        then(specialtyRepository).should(timeout(100)).findById(anyLong());
+
         //then(specialtyRepository).should(times(1)).findById(anyLong());
         then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
@@ -60,7 +63,8 @@ class SpecialitySDJpaServiceTest {
         service.deleteById(1l);
 
         //then
-        then(specialtyRepository).should(times(2)).deleteById(1L);
+        //then(specialtyRepository).should(times(2)).deleteById(1L);
+        then(specialtyRepository).should(timeout(100).times(2)).deleteById(1L);
     }
 
     @Test
@@ -100,5 +104,62 @@ class SpecialitySDJpaServiceTest {
         service.delete(new Speciality());
         //then
         then(specialtyRepository).should().delete(any());
+    }
+
+    @Test
+    void testDoThrow() {
+        doThrow(new RuntimeException("boom")).when(specialtyRepository).delete(any());
+        assertThrows(RuntimeException.class,()->service.delete(new Speciality()));
+        verify(specialtyRepository).delete(any());
+    }
+
+    @Test
+    void testFindByIdThrows() {
+        given(specialtyRepository.findById(1l)).willThrow(new RuntimeException("boom"));
+        assertThrows(RuntimeException.class,()->service.findById(1l));
+        then(specialtyRepository).should().findById(1l);
+    }
+
+    @Test
+    void testDeleteBDD() {
+        willThrow(new RuntimeException("boom")).given(specialtyRepository).delete(any());
+        assertThrows(RuntimeException.class,()->service.delete(new Speciality()));
+        then(specialtyRepository).should().delete(any());
+    }
+
+    @Test
+    void testSaveLambda() {
+        //given
+        final String MATCH_ME="MATCH_ME";
+        Speciality speciality=new Speciality();
+        speciality.setDescription(MATCH_ME);
+
+        Speciality savedSpecialty=new Speciality();
+        savedSpecialty.setId(1L);
+        given(specialtyRepository.save(argThat(argument->argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+
+        //when
+        Speciality returnedSpecialty = service.save(speciality);
+        //then
+        assertThat(returnedSpecialty.getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    void testSaveLambdaNoMatch() {
+        //given
+        final String MATCH_ME="MATCH_ME";
+        Speciality speciality=new Speciality();
+        speciality.setDescription("Not a match");
+
+        Speciality savedSpecialty=new Speciality();
+        savedSpecialty.setId(1L);
+        given(specialtyRepository.save(argThat(argument->argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+
+        //when
+        Speciality returnedSpecialty = service.save(speciality);
+        //then
+        assertNull(returnedSpecialty);
+
     }
 }
